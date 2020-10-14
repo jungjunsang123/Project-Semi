60cdf417a084e77be34820f6b91e928a00c12c0a
Mr.K <bcgrhio@naver.com>
2020-10-14 12:16:12 +0900

    test

--- I,Scream/src/org/project/model/MemberVO.java
+++ I,Scream/src/org/project/model/MemberVO.java
@@ -9,102 +9,6 @@ public class MemberVO {
 	private String regDate;
 	private String sex;
 	private String age;
-	
-	
-	
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getAddress() {
-		return address;
-	}
-
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public String getTel() {
-		return tel;
-	}
-
-
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-
-
-	public String getRegDate() {
-		return regDate;
-	}
-
-
-	public void setRegDate(String regDate) {
-		this.regDate = regDate;
-	}
-
-
-	public String getSex() {
-		return sex;
-	}
-
-
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-
-
-	public String getAge() {
-		return age;
-	}
-
-
-
-	public void setAge(String age) {
-		this.age = age;
-	}
-
-
-
-	public MemberVO() {
-		super();
-	}
-	
-
-	public MemberVO(String id, String password, String name) {
-		super();
-		this.id = id;
-		this.password = password;
-		this.name = name;
-	}
-
-
-
-
 	public MemberVO(String id, String password, String address, String name, String tel, String regDate, String sex,
 			String age) {
 		super();
@@ -117,7 +21,59 @@ public class MemberVO {
 		this.sex = sex;
 		this.age = age;
 	}
+	public MemberVO() {
+		super();
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
 	
 	
-
+	
+	
 }
